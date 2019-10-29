9d55105780829d58623a2e42187581d47d74e0ec
Stempreiss <jcstempreiss@outlook.com>
2019-10-29 09:17:42 -0600

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { Route, RouterModule } from '@angular/router';
+import { HttpClientModule }    from '@angular/common/http';
+
+const routes: Route[]= [
+  {path: 'home', component: HomeComponent},
+  {path: 'form', component: FormComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
